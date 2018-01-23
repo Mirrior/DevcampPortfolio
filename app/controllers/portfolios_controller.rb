5bba8a4de3a8f9e608e5dfacbb842f82e0bd051a
Rustin Player <rjplayer@outlook.com>
2018-01-23 14:16:31 -0700

    Added destroy funtion to portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -3,12 +3,16 @@ class PortfoliosController < ApplicationController
     @portfolio_items = Portfolio.all
   end
 
+  def show
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
   def new
     @portfolio_item = Portfolio.new
   end
-  
-  def show
-    @portfolio_item = Portfolio.find(params[:id])
+
+  def edit
+      @portfolio_item = Portfolio.find(params[:id])
   end
 
   def create
@@ -23,10 +27,6 @@ class PortfoliosController < ApplicationController
     end
   end
 
-  def edit
-      @portfolio_item = Portfolio.find(params[:id])
-  end
-
   def update
     @portfolio_item = Portfolio.find(params[:id])
     
@@ -39,4 +39,17 @@ class PortfoliosController < ApplicationController
     end
   end
 
+  def destroy
+    #Perfom the lookup
+    @portfolio_item = Portfolio.find(params[:id])
+
+    #Destroy/delete the record
+    @portfolio_item.destroy
+
+    #redirect
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'Record was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
 end
