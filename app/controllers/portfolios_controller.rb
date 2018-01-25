2afcdedbe58bf28c1ff8ccf22f055835fabff392
Rustin Player <rjplayer@outlook.com>
2018-01-25 16:20:19 -0700

    Integrated nested attributes for portfolio model

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -3,9 +3,9 @@ class PortfoliosController < ApplicationController
     @portfolio_items = Portfolio.all
   end
 
-  def angular
-    @angular_portfolio_items = Portfolio.angular
-  end
+  #def angular
+  #  @angular_portfolio_items = Portfolio.angular
+  #end
 
   def show
     @portfolio_item = Portfolio.find(params[:id])
