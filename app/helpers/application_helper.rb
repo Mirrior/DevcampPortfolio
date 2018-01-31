a9b72d8db4cea4f80237b47eec9ac2b102fa25a4
Rustin Player <rjplayer@outlook.com>
2018-01-31 15:45:04 -0700

    Implemented new gem into rails app for copyright

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting")
     end
   end
+
+  def copyright_generator
+    Player001ViewTool::Renderer.copyright 'Rustin Player', 'All rights reserved'
+  end
 end
