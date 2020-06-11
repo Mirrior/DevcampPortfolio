06e5669c9e6b71960262a4664f9677e6f13dad54
Rustin Player <rustinplayer@Rustins-MacBook-Pro.local>
2020-06-11 14:55:10 -0600

    Changed default page title

--- app/controllers/concerns/default_page_content.rb
+++ app/controllers/concerns/default_page_content.rb
@@ -1,12 +1,12 @@
 module DefaultPageContent
   extend ActiveSupport::Concern
-  
+
   included do
     before_action :set_page_defaults
   end
 
   def set_page_defaults
-    @page_title = "Devcamp Portfolio | My Portfolio Website"
-    @seo_keywords = "Rustin Player portfolio"
+    @page_title = "Rustin's Portfolio Website"
+    @seo_keywords = "Rustin Player Portfolio"
   end
 end
